9033deb1e52060025c776837e2ade42aff1f9885
ian mosonik <ian.mosonik@student.moringaschool.com>
2024-09-20 11:55:19 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your code here!
+// const element = document.createElement("div");
+// document.body.append(element);
+// const ul = document.createElement("ul");
+// for (let i = 0; i < 3; i++) {
+//     const li = document.createElement("li");
+//     li.textContent = (i + 1).toString();
+//     ul.append(li);
+//   }
+
+//   element.append(ul);
+
+let main = document.getElementById("main")
+// console.log(main)
+main.remove()
+
+let newHeader =document.createElement("h1")
+newHeader.setAttribute("id","victory")
+newHeader.textContent = "ian is the champion"
+
+
+  
